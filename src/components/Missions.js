85cd6cab8b2f39f0d38c3432efc8cf618f7e9994
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-02-20 16:26:48 -0300

    Requisito 8 feito

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions" />
+      <div data-testid="missions">
+        <Title headline="MISSÕES" />
+      </div>
     );
   }
 }
+
 export default Missions;
